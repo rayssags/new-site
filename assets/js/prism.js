11d47f9a57104ce17f30b12efb84478e56f2fb16
Rayssa Guimaraes Silva <rayssa17@astro.ufrj.br>
2025-10-27 18:15:43 +0100

    Testing new layout5

--- assets/js/prism.js
+++ assets/js/prism.js
@@ -1,11 +1,11 @@
 import Prism from "prismjs";
 
 {{ range $.Site.Params.prism.languages }}
-  import "prismjs/components/prism-{{ . }}";
+  import "node_modules/prismjs/components/prism-{{ . }}";
 {{ end }}
 
 {{ range $.Site.Params.prism.plugins }}
-  import "prismjs/plugins/{{ . }}/prism-{{ . }}";
+  import "node_modules/prismjs/plugins/{{ . }}/prism-{{ . }}";
 {{ end }}
 
 Prism.highlightAll();
